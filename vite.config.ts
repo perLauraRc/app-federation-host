03f47d52d79a63162859f282c3546b16d28da198
perLauraRc <lauraribesc@gmail.com>
2025-09-02 17:34:45 +0200

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config https://vitest.dev/config
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: '.vitest/setup',
+    include: ['**/test.{ts,tsx}']
+  },
+  server: {
+    host: 'react-ts-vitest-testing-library-tailwind-css-eslint-prettier',
+    port: 5174,
+    open: true,
+  },
+})
