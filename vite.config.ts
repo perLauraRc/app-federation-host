43f163bf7b5c9fb7f748717f34048380163a407e
perLauraRc <lauraribesc@gmail.com>
2025-09-04 12:29:08 +0200

    install vite-plug-federation plugin and setup federation module remote app

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import federation from '@originjs/vite-plugin-federation'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    federation({
+      name: 'host-app',
+      remotes: {
+        remoteApp: 'http://localhost:5174/assets/remoteEntry.js'
+      },
+      shared: ['react', 'react-dom']
+    })
+  ],
   test: {
     globals: true,
     environment: 'happy-dom',
@@ -14,7 +25,13 @@ export default defineConfig({
   },
   server: {
     host: 'react-ts-vitest-testing-library-tailwind-css-eslint-prettier',
-    port: 5174,
-    open: true,
+    port: 5173,
+    open: true
   },
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false, // Disables minification, making the output easier to debug
+    cssCodeSplit: false // Disables CSS combination into a single file rather than splitting it.
+  }
 })
