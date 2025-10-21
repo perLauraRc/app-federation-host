0d647a7045cbb2f002b98449f15db97cd9820a06
perLauraRc <lauraribesc@gmail.com>
2025-10-21 18:07:43 +0200

    add an environment value condition to assign a value to baseUrl for playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,9 +28,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: `${process.env.BASE_URL || BASE_URL}:${
-      process.env.HOST_PORT || HOST_PORT
-    }`,
+    baseURL: process.env.CI
+      ? 'http://localhost'
+      : `${process.env.BASE_URL || BASE_URL}:${
+          process.env.HOST_PORT || HOST_PORT
+        }`,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     /* Take screenshot on failure */
