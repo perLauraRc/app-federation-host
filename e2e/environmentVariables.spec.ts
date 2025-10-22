8e18b3bdab274e7845f339c1de3f90ed81ff01bb
perLauraRc <lauraribesc@gmail.com>
2025-10-22 17:03:02 +0200

    remove Environment Variables e2e testing when process.env.CI equals true

--- e2e/environmentVariables.spec.ts
+++ e2e/environmentVariables.spec.ts
@@ -23,7 +23,7 @@ test.describe('Environment Variables', () => {
       await expect(process.env.HOST_PORT).toMatch('5173')
       await expect(process.env.REMOTE_URL).toMatch('http://localhost')
       await expect(process.env.REMOTE_PORT).toMatch('5174')
-    } else {
+    } else if (!process.env.CI) {
       await expect(process.env.HOSTNAME).toMatch('app-federation')
       await expect(process.env.BASE_URL).toMatch('http://app-federation')
       await expect(process.env.HOST_PORT).toMatch('5173')
