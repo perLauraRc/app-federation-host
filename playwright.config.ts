d0067fee337fc454c01f764698d86fb8b96de0d2
perLauraRc <lauraribesc@gmail.com>
2025-10-22 18:49:35 +0200

    set reuseExistingServer uo for webserver in CI mode to true

--- playwright.config.ts
+++ playwright.config.ts
@@ -84,7 +84,7 @@ export default defineConfig({
         {
           command: 'npm run dev',
           url: 'http://localhost:5173',
-          reuseExistingServer: false,
+          reuseExistingServer: true,
           timeout: 2 * 60 * 1000
         }
       ]
